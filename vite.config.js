a5c5b123d28f368f5c79935cb54f19326ea281c2
Yohaan <yohanakipruto@gmail.com>
2025-03-11 17:42:42 +0300

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/website-rental-cars/',
+  base: '/yohaan-dev.github.io/website-rental-cars/',
   plugins: [react()],
   server: {
     port: 3000,
